d40485854c3accecc9121cb621a014cd0c36e332
Gabriel-rd2 <gabriel@rd2.ventures>
2023-11-08 23:47:21 -0300

    atualiza logs e run test

--- logs/run_test.sh
+++ logs/run_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Roda 3 clientes UDP, utiliando 6fps, 12fps e 24fps
-node udp_client --fps $2 --duration $1 --source ../videos/exemplo.mp4 > client_log-$2fps-$1s&
-node udp_client --fps $3 --duration $1 --source ../videos/exemplo2.mp4 > client_log2-$3fps-$1s&
-node udp_client --fps $4 --duration $1 > client_log3-$4fps-$1s&
+node udp_client --fps $2 --duration $1 --source ../videos/exemplo.mp4 > client_log1-$2fps-$1s.txt&
+node udp_client --fps $3 --duration $1 --source ../videos/exemplo2.mp4 > client_log2-$3fps-$1s.txt&
+node udp_client --fps $4 --duration $1 > client_log3-$4fps-$1s.txt&
 
 # O caracte '&' é para rodar os processos anteriores em background
 # O comando a seguir espera eles terminarem
