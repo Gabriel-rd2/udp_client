bc93eeadecd4705e951ce8988083f0501ee2e00c
Gabriel-rd2 <gabriel@rd2.ventures>
2023-11-08 23:40:26 -0300

    change run_test to work from inside logs folder

---
+++ logs/run_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Roda 3 clientes UDP, utiliando 6fps, 12fps e 24fps
+node udp_client --fps $2 --duration $1 --source ../videos/exemplo.mp4 > client_log-$2fps-$1s&
+node udp_client --fps $3 --duration $1 --source ../videos/exemplo2.mp4 > client_log2-$3fps-$1s&
+node udp_client --fps $4 --duration $1 > client_log3-$4fps-$1s&
+
+# O caracte '&' é para rodar os processos anteriores em background
+# O comando a seguir espera eles terminarem
+wait
+
+echo "Todos os clientes terminaram!"
